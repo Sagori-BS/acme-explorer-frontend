353d1c80da269de42821bd4bed4b840f0e63ba6b
David Bujosa <davidbujosa@gmail.com>
2022-04-04 07:02:52 +0200

    feat: restructure app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { NbPasswordAuthStrategy, NbAuthModule } from '@nebular/auth';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
