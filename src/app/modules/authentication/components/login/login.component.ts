9601685fa900cf48a2d2c5b72a4212cc203734c0
David Bujosa <davidbujosa@gmail.com>
2022-04-04 10:02:33 +0200

    feat: create login

---
+++ src/app/modules/authentication/components/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  ngOnInit(): void {}
+}
---
+++ src/app/modules/authentication/components/register/register.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  ngOnInit(): void {}
+}
