aa52255294b443de0ab9f2777455fbb620232252
Jose Sandoval <joseasandovalp@gmail.com>
2022-05-18 11:15:53 +0200

    feat: add prod variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'my-api-url' // Replace with your own API url
+  apiUrl: 'https://api-gateway-bujosa.cloud.okteto.net', // Replace with your own API url
+  paypalKey:
+    'AVyCK6NnP9t4gAk99fTfx9chhy76APxXVMGhXQ5HZK_2UnEOrdph9uc6K_MrIr_S10F6KfKUZPc_Qq2I'
 };
